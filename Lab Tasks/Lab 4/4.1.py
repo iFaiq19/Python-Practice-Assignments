fe6a51515598e47264c132b52c23f0d01cbfc6bc
ifaiq19 <faiq.019@gmail.com>
2020-06-18 15:14:43 +0500

    Lab Tasks

---
+++ Lab Tasks/Lab 4/4.1.py
@@ -0,0 +1,7 @@
+"""Write a single print statement to display the following text:
+    "I am displaying the use
+    of
+    various 'Escape Sequence'
+    Back Slash (\) is the 'escape character'." """
+
+print('\"I am displaying the use\nof\nvarious \'Escape Sequence\'\nBack Slash (\) is the \'escape character\'.\"')
---
+++ Lab Tasks/Lab 4/4.2.py
@@ -0,0 +1,3 @@
+# Convert the following equation into single python statement.
+
+print('((3 + 4*x)/5) - (10*(y - 5)(a + b + c)/x) + (9*(4/x + (9 + x)/y))')
